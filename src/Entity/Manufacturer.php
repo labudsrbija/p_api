7c02ce9be9c0a1886ce7913a1ad9d3f0e1ff149d
labudsrbija <stefanosfs90@gmail.com>
2022-05-16 01:29:26 +0200

    subresources

--- src/Entity/Manufacturer.php
+++ src/Entity/Manufacturer.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -59,6 +60,7 @@ class Manufacturer
      * @var Product[] Available products from this manufacturer
      * @ORM\OneToMany(targetEntity="Product", mappedBy="manufacturer", cascade={"persist", "remove"})
      */
+    #[ApiSubresource]
     private iterable $products;
 
     public function __construct()
